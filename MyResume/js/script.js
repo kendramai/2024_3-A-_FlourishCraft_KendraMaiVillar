c0848d6592671f7ccf712854718f1488b97bde29
Kendra Villar <157691288+kendramai@users.noreply.github.com>
2024-02-03 21:11:20 +0700

    Update script.js

--- MyResume/js/script.js
+++ MyResume/js/script.js
@@ -8,5 +8,19 @@ $(document).ready(function(){
     $(window).on('scroll load',function(){
         $('#menu').removeClass('fa-times');
         $('header').removeClassClass('toggle');
-    })
+    });
+
+    $('a[href*="#"]').on('click',function(e){
+        
+        e.preventDefault();
+
+        $('html, body').animate({
+
+            scrollTop : $($(this).attr('href')).offset().top,
+
+        },
+            500,
+            'linear'
+        );
+    });
 });
